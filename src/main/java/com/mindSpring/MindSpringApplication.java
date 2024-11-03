1cbc4c15c28a622c434f90bbb964e17370eb4c90
GyeongJoon <jjangjoon328@naver.com>
2024-11-03 18:28:22 +0900

    [init] setting

---
+++ src/main/java/com/mindSpring/MindSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mindSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindSpring/MindSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
