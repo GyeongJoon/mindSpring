61e75a0d564b9eb91103a30520a58749e964195e
GyeongJoon <jjangjoon328@naver.com>
2024-11-04 11:48:50 +0900

    [feat] MemberEntity 생성

---
+++ src/main/java/com/mindSpring/domain/entity/Member.java
@@ -0,0 +1,37 @@
+package com.mindSpring.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String gender;
+}
