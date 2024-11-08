d0abd973af649ecd5faab994b60ec23101c5575a
GyeongJoon <jjangjoon328@naver.com>
2024-11-08 12:02:39 +0900

    [feat] CounselorDto 생성

---
+++ src/main/java/com/mindSpring/domain/dto/CounselorRequestDto.java
@@ -0,0 +1,21 @@
+package com.mindSpring.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CounselorRequestDto {
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    private String career;
+}
---
+++ src/main/java/com/mindSpring/domain/dto/CounselorResponseDto.java
@@ -0,0 +1,23 @@
+package com.mindSpring.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CounselorResponseDto {
+
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    private String career;
+}
