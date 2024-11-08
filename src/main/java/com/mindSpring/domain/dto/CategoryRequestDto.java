383319bc5a90792d64b2effe744500f937e76769
GyeongJoon <jjangjoon328@naver.com>
2024-11-08 12:02:24 +0900

    [feat] CategoryDto 생성

---
+++ src/main/java/com/mindSpring/domain/dto/CategoryRequestDto.java
@@ -0,0 +1,15 @@
+package com.mindSpring.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryRequestDto {
+
+    private String name;
+}
