9583924f49a9cd45c91d5f9c53a12baa2decf667
GyeongJoon <jjangjoon328@naver.com>
2024-11-08 12:01:35 +0900

    [feat] ReviewDto 생성

---
+++ src/main/java/com/mindSpring/domain/dto/ReviewResponseDto.java
@@ -0,0 +1,19 @@
+package com.mindSpring.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReviewResponseDto {
+
+    private Long id;
+
+    private int rating;
+
+    private String content;
+}
