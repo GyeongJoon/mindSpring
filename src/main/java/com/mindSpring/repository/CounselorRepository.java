e04ec28b0b27bef9cbab653510d8f02b40936775
GyeongJoon <jjangjoon328@naver.com>
2024-11-09 11:51:19 +0900

    [feat] CounselorRepository 생성

---
+++ src/main/java/com/mindSpring/repository/CounselorRepository.java
@@ -0,0 +1,9 @@
+package com.mindSpring.repository;
+
+import com.mindSpring.domain.entity.Counselor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CounselorRepository extends JpaRepository<Counselor, Long> {
+}
