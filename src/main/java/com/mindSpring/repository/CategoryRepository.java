edccfc59573d97d833552c4123ee398f344b2acb
GyeongJoon <jjangjoon328@naver.com>
2024-11-09 11:50:52 +0900

    [feat] CategoryRepository 생성

---
+++ src/main/java/com/mindSpring/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.mindSpring.repository;
+
+import com.mindSpring.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/mindSpring/service/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.mindSpring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryRepository {
+
+    private final CategoryRepository categoryRepository;
+
+    // 카테고리 전체 조회 API
+}
