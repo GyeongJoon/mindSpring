08973085d94a1ade01b6d0f6f17cc4a328ba3550
GyeongJoon <jjangjoon328@naver.com>
2024-11-09 11:51:36 +0900

    [feat] MemberRepository 생성

---
+++ src/main/java/com/mindSpring/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.mindSpring.repository;
+
+import com.mindSpring.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
