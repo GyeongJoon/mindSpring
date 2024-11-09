7cfce972299426630e529db966058df3dc20625d
GyeongJoon <jjangjoon328@naver.com>
2024-11-09 11:51:49 +0900

    [feat] ReviewRepository 생성

---
+++ src/main/java/com/mindSpring/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.mindSpring.repository;
+
+import com.mindSpring.domain.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
