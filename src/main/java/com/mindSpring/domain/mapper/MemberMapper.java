9b80cb44a71ae6a0aa79819e61b0e4b0a28c2b78
GyeongJoon <jjangjoon328@naver.com>
2024-11-13 11:22:28 +0900

    [feat] MemberMapper 수정

--- src/main/java/com/mindSpring/domain/mapper/MemberMapper.java
+++ src/main/java/com/mindSpring/domain/mapper/MemberMapper.java
@@ -1,20 +1,20 @@
 package com.mindSpring.domain.mapper;
 
-import com.mindSpring.domain.dto.MemberRequestDto;
+import com.mindSpring.domain.dto.SignupRequestDto;
 import com.mindSpring.domain.dto.MemberResponseDto;
 import com.mindSpring.domain.entity.Member;
 
 public class MemberMapper {
 
     // Dto -> Entity
-    public static Member toMemberEntity(MemberRequestDto memberRequestDto) {
+    public static Member toMemberEntity(SignupRequestDto signupRequestDto) {
         return Member.builder()
-                .name(memberRequestDto.getName())
-                .email(memberRequestDto.getEmail())
-                .password(memberRequestDto.getPassword())
-                .phone(memberRequestDto.getPhone())
-                .age(memberRequestDto.getAge())
-                .gender(memberRequestDto.getGender())
+                .name(signupRequestDto.getName())
+                .email(signupRequestDto.getEmail())
+                .password(signupRequestDto.getPassword())
+                .phone(signupRequestDto.getPhone())
+                .age(signupRequestDto.getAge())
+                .gender(signupRequestDto.getGender())
                 .build();
     }
 
