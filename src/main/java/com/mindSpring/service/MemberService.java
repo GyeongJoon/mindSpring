3b148b0b5887f03c562357f4e02df3aff04a485f
GyeongJoon <jjangjoon328@naver.com>
2024-11-14 16:02:26 +0900

    [feat] MemberService 수정

--- src/main/java/com/mindSpring/service/MemberService.java
+++ src/main/java/com/mindSpring/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.mindSpring.service;
 
-import com.mindSpring.domain.dto.MemberRequestDto;
+import com.mindSpring.domain.dto.LoginRequestDto;
+import com.mindSpring.domain.dto.SignupRequestDto;
 import com.mindSpring.domain.dto.MemberResponseDto;
 import com.mindSpring.domain.entity.Member;
 import com.mindSpring.domain.mapper.MemberMapper;
@@ -28,24 +29,39 @@ public class MemberService {
 
     // 회원 등록 API
     @Transactional
-    public MemberResponseDto createMember(MemberRequestDto memberRequestDto) {
-        memberRepository.findByEmail(memberRequestDto.getEmail())
+    public MemberResponseDto createMember(SignupRequestDto signupRequestDto) {
+        memberRepository.findByEmail(signupRequestDto.getEmail())
                 .ifPresent(member -> {
                     throw new appException(ErrorCode.MEMBER_ALREADY_EXISTS);
                 });
 
-        Member memberEntity = MemberMapper.toMemberEntity(memberRequestDto);
+        Member memberEntity = MemberMapper.toMemberEntity(signupRequestDto);
         memberRepository.save(memberEntity);
         return MemberMapper.toMemberDto(memberEntity);
     }
 
+    // 회원 로그인 API
+    @Transactional(readOnly = true)
+    public MemberResponseDto login(LoginRequestDto loginRequestDto) {
+        Member member = memberRepository.findByEmail(loginRequestDto.getEmail())
+                .orElseThrow(() -> new appException(ErrorCode.MEMBER_NOT_FOUND));
+
+        // 비밀번호 검증
+        if (!member.getPassword().equals(loginRequestDto.getPassword())) {
+            throw new appException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        // 로그인 성공 메시지 반환
+        return MemberMapper.toMemberDto(member);
+    }
+
     // 회원 수정 API
     @Transactional
-    public MemberResponseDto updateMember(Long memberId, MemberRequestDto memberRequestDto) {
+    public MemberResponseDto updateMember(Long memberId, SignupRequestDto signupRequestDto) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new appException(ErrorCode.MEMBER_NOT_FOUND));
 
-        member.updateMemberInfo(memberRequestDto);
+        member.updateMemberInfo(signupRequestDto);
         return MemberMapper.toMemberDto(member);
     }
 
@@ -57,8 +73,4 @@ public class MemberService {
 
         memberRepository.delete(member);
     }
-
-    // 회원 로그인 API
-    @Transactional
-    public void
 }
