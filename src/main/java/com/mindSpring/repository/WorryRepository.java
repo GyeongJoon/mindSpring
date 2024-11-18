bdaae14434be1595144f8ba1233592f3d19c67fa
GyeongJoon <jjangjoon328@naver.com>
2024-11-18 15:41:41 +0900

    [feat] 삭제

--- src/main/java/com/mindSpring/repository/WorryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mindSpring.repository;
-
-import com.mindSpring.domain.entity.Worry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorryRepository extends JpaRepository<Worry, Long> {
-}
