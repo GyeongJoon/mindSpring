ce0cf81e10b976c659d72471be006bdbc6c0d603
GyeongJoon <jjangjoon328@naver.com>
2024-11-29 13:05:36 +0900

    refactor: data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO category (NAME) VALUES ('COURSE');
 INSERT INTO category (NAME) VALUES ('FAMILY');
 INSERT INTO category (NAME) VALUES ('OTHER');
 
-INSERT INTO member (EMAIL, NAME, PASSWORD, AGE, GENDER, JOB) VALUES ('jjangjoon@naver,com', '장경준', '1234', '24', '남성', '학생');
+INSERT INTO member (EMAIL, NAME, PASSWORD, AGE, GENDER, JOB) VALUES ('jjangjoon@naver.com', '장경준', '1234', '24', '남성', '학생');
 
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('김현우', '20', '남성', '컴공과 2학년');
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('장경준', '24', '남성', '미소과 3학년');
