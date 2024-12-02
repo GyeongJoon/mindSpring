d5e99a53d6557fef4dde1858acdab7d4db678b70
GyeongJoon <jjangjoon328@naver.com>
2024-12-02 14:26:34 +0900

    update

---
+++ src/main/java/com/mindSpring/domain/member/dto/SignupResponseDto.java
@@ -0,0 +1,13 @@
+package com.mindSpring.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupResponseDto {
+
+    private String message;
+}
