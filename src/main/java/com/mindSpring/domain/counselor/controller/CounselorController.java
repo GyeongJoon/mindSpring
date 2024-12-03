3d00c763a8ba6984951f17968a168a7878519a1a
GyeongJoon <jjangjoon328@naver.com>
2024-12-03 13:06:16 +0900

    refactor: CounselorController

--- src/main/java/com/mindSpring/domain/counselor/controller/CounselorController.java
+++ src/main/java/com/mindSpring/domain/counselor/controller/CounselorController.java
@@ -1,12 +1,15 @@
 package com.mindSpring.domain.counselor.controller;
 
+import com.mindSpring.common.ResponseMessage;
 import com.mindSpring.domain.counselor.dto.CounselorResponseDto;
 import com.mindSpring.domain.counselor.service.CounselorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,16 +21,28 @@ public class CounselorController implements CounselorControllerSwagger{
     // 상담사 전체 조회
     @Override
     @GetMapping
-    public ResponseEntity<List<CounselorResponseDto>> getCounselors(@PathVariable("memberId") Long memberId) {
+    public ResponseEntity<ResponseMessage<Object>> getCounselors(@PathVariable("memberId") Long memberId) {
         List<CounselorResponseDto> counselors = counselorService.getCounselors(memberId);
-        return ResponseEntity.ok(counselors);
+
+        ResponseMessage<Object> response = new ResponseMessage<>(
+                "success",
+                "상담사 전체 조회 성공",
+                counselors
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // 상담사 조회
     @Override
     @GetMapping("/{counselorId}")
-    public ResponseEntity<CounselorResponseDto> getCounselor(@PathVariable("memberId") Long memberId, @PathVariable("counselorId") Long counselorId) {
+    public ResponseEntity<ResponseMessage<Object>> getCounselor(@PathVariable("memberId") Long memberId, @PathVariable("counselorId") Long counselorId) {
         CounselorResponseDto counselor = counselorService.getCounselor(memberId, counselorId);
-        return ResponseEntity.ok(counselor);
+
+        ResponseMessage<Object> response = new ResponseMessage<>(
+                "success",
+                "상담사 조회 성공",
+                counselor
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
