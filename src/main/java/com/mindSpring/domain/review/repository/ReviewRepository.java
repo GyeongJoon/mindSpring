a4cf3a6d1e3a6eee4c1b9f5d43a2e1323b513611
GyeongJoon <jjangjoon328@naver.com>
2024-12-03 13:07:38 +0900

    refactor: Review

--- src/main/java/com/mindSpring/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/mindSpring/domain/review/repository/ReviewRepository.java
@@ -2,8 +2,16 @@ package com.mindSpring.domain.review.repository;
 
 import com.mindSpring.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 상담사별 리뷰 조회
+    @Query("SELECT r FROM Review r WHERE r.counselor.id = :counselorId")
+    List<Review> findByCounselorId(@Param("counselorId") Long counselorId);
 }
