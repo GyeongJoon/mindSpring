b6b9577d1599e20702e8b4501ab8e5a2700eeb7c
GyeongJoon <jjangjoon328@naver.com>
2024-12-03 12:14:07 +0900

    feat: ResponseMessage

---
+++ src/main/java/com/mindSpring/common/ResponseMessage.java
@@ -0,0 +1,20 @@
+package com.mindSpring.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseMessage<T> {
+    private String status; // success 또는 error
+    private String message; // 사용자 메시지
+    private T data; // 응답 데이터
+
+    public ResponseMessage(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    // Getters and setters...
+}
