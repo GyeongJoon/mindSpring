55e2090df0da110fa8dc4f0b7cd21d55bee004b2
GyeongJoon <jjangjoon328@naver.com>
2024-12-04 09:51:23 +0900

    refactor: update

--- src/main/java/com/mindSpring/domain/openAi/repository/OpenAiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mindSpring.domain.openAi.repository;
-
-import com.mindSpring.domain.openAi.entity.OpenAi;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OpenAiRepository extends JpaRepository<OpenAi, Long> {
-}
