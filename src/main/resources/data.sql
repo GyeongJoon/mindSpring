101997f78f53a5c25d37acb4c6742a278dfce905
GyeongJoon <jjangjoon328@naver.com>
2024-12-09 16:43:56 +0900

    refactor: update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,4 @@ INSERT INTO member (EMAIL, NAME, PASSWORD, AGE, GENDER, JOB) VALUES ('jjangjoon@
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('김현우', '20', '남성', '컴공과 2학년', '4년 경력을 토대로 진솔한 소통을 통해 신뢰를 형성하고, 공감과 조언을 바탕으로 삼는 상담입니다.');
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('장경준', '24', '남성', '미소과 3학년', '#또래상담병출신 #국내유일상담방법 #노벨상담상수상 #상담고객10000명돌파');
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('허수진', '24', '여성', '미소과 4학년', '#경력5년차베테랑 #공감여왕? #비공식심리치료사 #상담받으세요');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('황인우', '20', '남성', '컴공과 3학년', '');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('황인우', '20', '남성', '컴공과 3학년', '#경력1년차 #진심상담 #자격증다수보유 #상담풀예약');
