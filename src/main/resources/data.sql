84d6a8b181ceb16ec3c411de8736abdb272a7931
GyeongJoon <jjangjoon328@naver.com>
2024-12-09 16:02:08 +0900

    refactor: update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO category (NAME) VALUES ('OTHER');
 
 INSERT INTO member (EMAIL, NAME, PASSWORD, AGE, GENDER, JOB) VALUES ('jjangjoon@naver.com', '장경준', '1234', '24', '남성', '학생');
 
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('김현우', '20', '남성', '컴공과 2학년');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('장경준', '24', '남성', '미소과 3학년');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('허수진', '24', '여성', '미소과 4학년');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER) VALUES ('황인우', '20', '남성', '컴공과 3학년');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('김현우', '20', '남성', '컴공과 2학년', '');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('장경준', '24', '남성', '미소과 3학년', '');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('허수진', '24', '여성', '미소과 4학년', '');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('황인우', '20', '남성', '컴공과 3학년', '');
