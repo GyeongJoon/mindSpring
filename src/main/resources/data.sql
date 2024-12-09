ea5b146017a9b5d5a541a28d112c010f3991447d
GyeongJoon <jjangjoon328@naver.com>
2024-12-09 16:15:55 +0900

    refactor: update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO category (NAME) VALUES ('OTHER');
 
 INSERT INTO member (EMAIL, NAME, PASSWORD, AGE, GENDER, JOB) VALUES ('jjangjoon@naver.com', '장경준', '1234', '24', '남성', '학생');
 
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('김현우', '20', '남성', '컴공과 2학년', '');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('장경준', '24', '남성', '미소과 3학년', '');
-INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('허수진', '24', '여성', '미소과 4학년', '');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('김현우', '20', '남성', '컴공과 2학년', '4년 경력을 토대로 진솔한 소통을 통해 신뢰를 형성하고, 공감과 조언을 바탕으로 삼는 상담입니다.');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('장경준', '24', '남성', '미소과 3학년', '#또래상담병출신 #국내유일상담방법 #노벨상담상수상 #상담고객10000명돌파');
+INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('허수진', '24', '여성', '미소과 4학년', '#경력5년차베테랑 #공감여왕? #비공식심리치료사 #상담받으세요');
 INSERT INTO counselor (NAME, AGE, GENDER, CAREER, EXPERIENCE) VALUES ('황인우', '20', '남성', '컴공과 3학년', '');
