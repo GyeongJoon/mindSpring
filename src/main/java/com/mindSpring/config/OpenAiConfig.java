5a043e2d6228950d9e82994adc8eff9442077aa5
GyeongJoon <jjangjoon328@naver.com>
2024-12-12 18:02:34 +0900

    style: OpenAiConfig

---
+++ src/main/java/com/mindSpring/config/OpenAiConfig.java
@@ -0,0 +1,25 @@
+package com.mindSpring.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAiConfig {
+
+    @Value("${openai.api.url}")
+    private String apiUrl;
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(apiUrl)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .build();
+    }
+}
